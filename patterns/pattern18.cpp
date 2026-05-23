e8eda15056547cdcf9c41a02ee57090f3191286d
CodeLens AI <codelens@google.com>
2026-05-23 23:08:12 +0530

    fix: Hardcoded Character 'E' Limits Pattern Flexibility

--- patterns/pattern18.cpp
+++ patterns/pattern18.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 void printpattern(int n){
     for(int i=0;i<n;i++){
-        for(char j='E'-i;j<='E';j++){
+        // Assuming the pattern should scale with n, printing characters up to 'A' + n - 1
+        // If the intention is to always end at 'E', then n should be <= 5.
+        char start_char = 'A' + (n - 1) - i;
+        char end_char = 'A' + (n - 1);
+        for(char j=start_char; j<=end_char; j++){
             cout<<j<<" ";
         }
         cout<<endl;
